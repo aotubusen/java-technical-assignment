c0468c94e92842afda98ab035b9728a35edf3ff3
adeyemi.otubusen <adeyemi.otubusen@paconsulting.com>
2022-05-10 23:41:50 +0100

    test case - shouldReturnZeroWhenNoItems (pass) - shouldDiscountPriceByHalf

--- src/test/java/kata/supermarket/discount/Buy1Get1FreeTest.java
+++ src/test/java/kata/supermarket/discount/Buy1Get1FreeTest.java
@@ -7,6 +7,7 @@ import java.util.List;
 import org.junit.jupiter.api.Test;
 
 import kata.supermarket.Item;
+import kata.supermarket.Product;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -19,5 +20,15 @@ class Buy1Get1FreeTest {
         Buy1Get1Free bogof = new Buy1Get1Free();
 
         assertEquals(BigDecimal.ZERO, bogof.calculate(items));
+    }
+    @Test
+    void shouldDiscountPriceByHalf(){
+        List<Item> items = new ArrayList<>();
+        Product product = new Product(new BigDecimal(2));
+        Buy1Get1Free bogof = new Buy1Get1Free();
+        items.add(product.oneOf());
+        items.add(product.oneOf());
+
+        assertEquals(2, bogof.calculate(items).doubleValue());
     }
 }
